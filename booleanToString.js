791e6bf18a8cb9d13f84460b0433284b7cc82643
YJordan-1 <yakeemjordan1@gmail.com>
2022-12-11 14:26:57 -0500

    codewars algo added

---
+++ booleanToString.js
@@ -0,0 +1,12 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Answer //
+
+function booleanToString(b) {
+  // convert boolean into a string
+  if (b) {
+    return true.toString();
+  } else {
+    return false.toString();
+  }
+}
