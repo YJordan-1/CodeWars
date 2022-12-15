383b2610e688ea796517751b64a90d2790ca0218
YJordan-1 <yakeemjordan1@gmail.com>
2022-12-15 11:32:57 -0600

    remove the spaces from the string, then return the resultant string

---
+++ removeStringSpaces.js
@@ -0,0 +1,15 @@
+/*
+DESCRIPTION
+Simple, remove the spaces from the string, then return the resultant string.
+*/
+
+// ANSWER
+
+function noSpace(x) {
+  // split the string into an array of words
+  let split = x.split(" ");
+
+  // join the array of words into a single string
+  let newWord = split.join("");
+  return newString;
+}
