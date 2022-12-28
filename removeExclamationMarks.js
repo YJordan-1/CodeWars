1caba8f88ebbe7e0ea502e61f77fbe03ab9ae3ae
YJordan-1 <yakeemjordan1@gmail.com>
2022-12-28 08:22:47 -0600

    remove exclamation marks from a given string

---
+++ removeExclamationMarks.js
@@ -0,0 +1,10 @@
+/*
+DESCRIPTION
+
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+*/
+
+// ANSWER
+
+const removeExclamationMarks = (s) => s.replace(/[!]/gi, "");
